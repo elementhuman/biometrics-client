e843f789e0826ba2dce93b4a8ad2b529e84ca75e
TariqAHassan <tariqaahassan@gmail.com>
2020-06-09 16:08:25 -0300

    Add test for `apply_and_wait()`

--- tests/test_client.py
+++ tests/test_client.py
@@ -9,6 +9,10 @@ from urllib.parse import urljoin
 from biometrics_client import client
 from tests import TEST_VIDEO_PATH
 
+FAKE_TASK_ID: str = "fake-task-id"
+APPLY_PAYLOAD = dict(response=dict(task_id=FAKE_TASK_ID), version="99")
+RESULTS_PAYLOAD = dict(response=dict(frames={}, summary={}), version="99")
+
 
 @responses.activate
 def test_ping(dummy_ehb_client: client.ElementHumanBiometrics) -> None:
@@ -24,24 +28,38 @@ def test_ping(dummy_ehb_client: client.ElementHumanBiometrics) -> None:
 
 @responses.activate
 def test_apply(dummy_ehb_client: client.ElementHumanBiometrics) -> None:
-    payload = dict(response=dict(task_id="fake-task-id"), version="99")
     responses.add(
         responses.POST,
         url=urljoin(dummy_ehb_client.url, "apply"),
-        json=payload,
+        json=APPLY_PAYLOAD,
         status=200,
     )
-    assert dummy_ehb_client.apply(TEST_VIDEO_PATH) == payload
+    assert dummy_ehb_client.apply(TEST_VIDEO_PATH) == APPLY_PAYLOAD
 
 
 @responses.activate
 def test_results(dummy_ehb_client: client.ElementHumanBiometrics) -> None:
-    fake_task_id: str = "fake-task-id"
-    payload = dict(response=dict(frames={}, summary={}), version="99")
     responses.add(
         responses.GET,
-        url=urljoin(dummy_ehb_client.url, f"results/{fake_task_id}"),
-        json=payload,
+        url=urljoin(dummy_ehb_client.url, f"results/{FAKE_TASK_ID}"),
+        json=RESULTS_PAYLOAD,
         status=200,
     )
-    assert dummy_ehb_client.results(fake_task_id) == payload
+    assert dummy_ehb_client.results(FAKE_TASK_ID) == RESULTS_PAYLOAD
+
+
+@responses.activate
+def test_apply_and_wait(dummy_ehb_client: client.ElementHumanBiometrics) -> None:
+    responses.add(
+        responses.POST,
+        url=urljoin(dummy_ehb_client.url, "apply"),
+        json=APPLY_PAYLOAD,
+        status=200,
+    )
+    responses.add(
+        responses.GET,
+        url=urljoin(dummy_ehb_client.url, f"results/{FAKE_TASK_ID}"),
+        json=RESULTS_PAYLOAD,
+        status=200,
+    )
+    assert dummy_ehb_client.apply_and_wait(TEST_VIDEO_PATH) == (FAKE_TASK_ID, RESULTS_PAYLOAD)
