dea59456b19f752790cc999d6b49a095fe696379
TariqAHassan <tariqaahassan@gmail.com>
2020-06-09 16:09:04 -0300

    Increase cov-fail-under

--- tests/run.sh
+++ tests/run.sh
@@ -1,6 +1,6 @@
 pytest --strict-markers \
        --cov=biometrics_client \
        --cov-report term-missing \
-       --cov-fail-under 80 \
+       --cov-fail-under 85 \
        --mypy \
        --showlocals -vv
