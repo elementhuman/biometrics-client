12ba8c9f23b9b401c579ab1fe153fc61a548e2ed
TariqAHassan <tariqaahassan@gmail.com>
2020-07-09 11:16:51 -0300

    Test credentials are correct

--- tests/__init__.py
+++ tests/__init__.py
@@ -8,3 +8,7 @@ from pathlib import Path
 
 TEST_VIDEO_PATH = Path("tests/_fixtures/camera_200_2200.webm").absolute()
 assert TEST_VIDEO_PATH.exists()
+
+
+DUMMY_ACCESS_KEY = "SOME-ACCESS-KEY"
+DUMMY_SECRET_KEY = "SOME-SECRET-KEY"
